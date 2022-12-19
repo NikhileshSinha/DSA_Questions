29655ba31f4c0bbeecb406c649c3980992c8c2ab
noobshubham <55689986+noobshubham@users.noreply.github.com>
2022-12-19 21:19:52 +0530

    add: Code Sol

---
+++ HackerRank/Running Time of Algorithms/shubham-sol.java
@@ -0,0 +1,26 @@
+class Result {
+
+    /*
+     * Complete the 'runningTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int runningTime(List<Integer> arr) {
+        // Write your code here
+        int count = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            int key = arr.get(i);
+            int j = i - 1;
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j+1, arr.get(j));
+                count++;
+                j--;
+            }
+            arr.set(j+1, key);
+        }
+        return count;
+    }
+
+}
